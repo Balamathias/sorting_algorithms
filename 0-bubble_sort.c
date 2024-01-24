ef25e96531785bd16da5bb6ddd6d5986d13fda57
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-01-23 17:51:47 -0800

    uploading and fixing errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,20 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
+ * bubble_sort - sort array elements in ascending order.
  * @array: array
  * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, idx, temp = 0;
+	size_t i = size, idx, temp = 0;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (idx = 0; idx < size; idx++)
+
+	for (i = 0; i--)
+		for (idx = 0; idx < i - 1; idx++)
 		{
 			if (array[idx] > array[idx + 1] && array[idx + 1])
 			{
