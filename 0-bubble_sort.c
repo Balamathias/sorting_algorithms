46abb9cb687ea30de54a9d43fe6cd9b416f17132
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-01-23 18:22:28 -0800

    uploading and fixing errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,19 +8,19 @@
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i = size, idx, temp = 0;
+	size_t i, idx, temp = 0;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
-	for (idx = 0; idx < i - 1; idx++)
-	{
-		if (array[idx] > array[idx + 1] && array[idx + 1])
+	for (i = 0; i < size; i++)
+		for (idx = 0; idx < size; idx++)
 		{
-			temp = array[idx];
-			array[idx] = array[idx + 1];
-			array[idx + 1] = temp;
-			print_array(array, size);
+			if (array[idx] > array[idx + 1] && array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+			}
 		}
-	}
-	i--;
 }
